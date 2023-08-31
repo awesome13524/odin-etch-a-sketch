6a5f6910590e22381d5fead3bac73119265dad14
awesome13524 <94251418+awesome13524@users.noreply.github.com>
2023-08-31 13:11:55 -0400

    Add button to change grid size

--- js/eas.js
+++ js/eas.js
@@ -1,19 +1,25 @@
 container = document.querySelector('.container');
-let row
-let box
+btn = document.querySelector('button');
+let row;
+let box;
+let allRows;
+let allBoxes;
 makeGrid(16);
-
-
-let elements = document.querySelectorAll('.box');
-
-elements.forEach((element) => {
+btn.addEventListener('click', buttonPress);
+allBoxes = document.querySelectorAll('.box');
+allBoxes.forEach((element) => {
     element.addEventListener('mouseover', () => {
         element.classList.add('hover');
     });
 });
 
 function makeGrid(gridLength) {
+    allRows = document.querySelectorAll('.row');
+        allRows.forEach((singleRow) => {
+            singleRow.remove();
+        });
     for(let i = 0; i < gridLength; i++){
+        
         row = document.createElement('div');
         row.classList.add('row');
         for(let j = 0; j < gridLength; j++){
@@ -22,5 +28,23 @@ function makeGrid(gridLength) {
             row.appendChild(box);
         }
         container.appendChild(row);
-    }    
+    }
+    allBoxes = document.querySelectorAll('.box');
+    allBoxes.forEach((element) => {
+        element.addEventListener('mouseover', () => {
+            element.classList.add('hover');
+        });
+    });
+}
+
+function buttonPress(){
+    let userInput = 0
+    while(userInput < 1 || userInput > 100){
+        userInput = prompt('Grid length can be from 1 to 100', "");
+        if(userInput === null){
+            return;
+        }
+        userInput = parseInt(userInput)
+    }
+    makeGrid(userInput);
 }
